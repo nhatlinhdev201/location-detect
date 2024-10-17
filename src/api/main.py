b8479288368c13c7df4c9fc17953bfc79e257fd5
Nhat Linh <nhatlinhdev201@gmail.com>
2024-10-17 11:33:36 +0700

    up

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from src.api.search import router as search_router
+# from src.api.test import router as test_router
 
 api_router = APIRouter()
 api_router.include_router(search_router, prefix="/search")
+# test_router.include_router(test_router, prefix="/test")
--- src/api/search.py
+++ src/api/search.py
@@ -13,9 +13,23 @@ with open('src/data/DataLocation.json', 'r', encoding='utf-8') as json_file:
 class SearchRequest(BaseModel):
     input: str
 
-@router.get('/')
+@router.post('/')
 async def search(request: SearchRequest):
     user_input = request.input
+    if not user_input:
+        raise HTTPException(status_code=400, detail="Input không hợp lệ")
+
+    # Tiền xử lý input
+    formatted_input = format_address(user_input)
+
+    # Tìm kiếm và lấy kết quả
+    best_matches = find_best_matches(formatted_input, data)
+
+    return best_matches
+
+@router.get('/locations')
+async def search_location(q: str = None):
+    user_input = q
     if not user_input:
         raise HTTPException(status_code=400, detail="Input không hợp lệ")
 
