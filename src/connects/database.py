c1260f7ae58246b2faab4ebafef19913b36a2eb3
Nhat Linh <nhatlinhdev201@gmail.com>
2024-10-29 13:54:23 +0700

    new

--- src/connects/database.py
+++ src/connects/database.py
@@ -1,11 +1,20 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 load_dotenv()
 
-MONGO_URI = os.getenv('MONGO_URI')
-DATABASE_NAME = os.getenv('DATABASE_NAME')
+username = os.getenv('MONGO_USERNAME')
+password = os.getenv('MONGO_PASSWORD')
+database_name = os.getenv('DATABASE_NAME')
+
+# Mã hóa tên người dùng và mật khẩu
+encoded_username = quote_plus(username)
+encoded_password = quote_plus(password)
+
+# Xây dựng MONGO_URI
+MONGO_URI = f"mongodb://{encoded_username}:{encoded_password}@localhost:27017"
 
 class MongoDB:
     def __init__(self, uri: str, db_name: str):
@@ -15,5 +24,4 @@ class MongoDB:
     async def get_collection(self, collection_name: str):
         return self.db[collection_name]
 
-mongo_db = MongoDB(MONGO_URI, DATABASE_NAME)
-
+mongo_db = MongoDB(MONGO_URI, database_name)
