1aa25c4950086c23013389e8148f425638c3c54a
Nhat Linh <nhatlinhdev201@gmail.com>
2024-10-29 15:17:41 +0700

    change port

--- src/connects/database.py
+++ src/connects/database.py
@@ -8,13 +8,14 @@ load_dotenv()
 username = os.getenv('MONGO_USERNAME')
 password = os.getenv('MONGO_PASSWORD')
 database_name = os.getenv('DATABASE_NAME')
+host = os.getenv('MONGO_HOST')
 
 # Mã hóa tên người dùng và mật khẩu
 encoded_username = quote_plus(username)
 encoded_password = quote_plus(password)
 
 # Xây dựng MONGO_URI
-MONGO_URI = f"mongodb://{encoded_username}:{encoded_password}@localhost:27017"
+MONGO_URI = f"mongodb://{encoded_username}:{encoded_password}@localhost:{host}"
 
 class MongoDB:
     def __init__(self, uri: str, db_name: str):
